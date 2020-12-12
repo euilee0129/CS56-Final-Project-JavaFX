8b5dcf9e26c6964bd7fdfc286f8ebba37d69fcdb
euilee0129 <superbankaisam@gmail.com>
2020-12-12 00:05:55 +0000

    added button to send to console

--- Box.java
+++ Box.java
@@ -17,58 +17,73 @@ public class Box extends Main {
 
 	public static void display(String str) {
 		Stage window = new Stage();
-		
+
 		Label label = new Label("");
+		label.setAlignment(Pos.TOP_CENTER);
+
+		// keyboard output
+		switch (str) {
+		case "C":
+			label = new Label("C");
+			
+			break;
+		case "C#":
+			label = new Label("C#");
+			break;
+		case "D":
+			label = new Label("D");
+			break;
+		case "D#":
+			label = new Label("D#");
+			break;
+		case "E":
+			label = new Label("E");
+			break;
+		case "F":
+			label = new Label("F");
+			break;
+		case "F#":
+			label = new Label("F#");
+			break;
+		case "G":
+			label = new Label("G");
+			break;
+		case "G#":
+			label = new Label("G#");
+			break;
+		case "A":
+			label = new Label("A");
+			break;
+		case "A#":
+			label = new Label("A#");
+			break;
+		case "B":
+			label = new Label("B");
+			break;
+		}
 		
 		
-		//------------
-		   switch(str) {
-         case "C" :
-        	 label = new Label("C");
-            break;
-         case "C#" :
-        	 label = new Label("C#");
-            break;
-         case "D" :
-        	 label = new Label("D");
-            break;
-         case "D#" :
-        	 label = new Label("D#");
-            break;
-         case "E" :
-        	 label = new Label("E");
-            break;
-         case "F" :
-        	 label = new Label("F");
-            break;
-         case "F#" :
-        	 label = new Label("F#");
-            break;
-         case "G" :
-        	 label = new Label("G");
-            break;
-         case "G#" :
-        	 label = new Label("G#");
-            break;
-         case "A" :
-        	 label = new Label("A");
-            break;
-         case "A#" :
-        	 label = new Label("A#");
-            break;
-         case "B" :
-        	 label = new Label("B");
-            break;
-		   }
-            
-		 ///------
+		/* button 1 */
+
+		Button btn = new Button();
+		btn.setText("send to console");
+		btn.setAlignment(Pos.BOTTOM_RIGHT);
+		btn.setMaxSize(100, 50);
+		btn.setTranslateX(90);
+		btn.setTranslateY(70);
+
+		btn.setOnAction(e -> {
+			System.out.println("Received from Box : "+ str );
+			// add close program button
+			
+		});
+
 		
-		
-		
+
 		StackPane root = new StackPane();
 		Scene scene = new Scene(root, 300, 250);
 
-		
+		root.getChildren().add(btn);
 		root.getChildren().add(label);
 
 		window.setScene(scene);
